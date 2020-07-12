57a4da9552710ac6326d760dde696faa0d7aff16
Paco Rus <pacorus21@gmail.com>
2020-07-12 02:08:39 +0200

    basic frontend, updated favicon, added logo

--- source/server/index.js
+++ source/server/index.js
@@ -1,7 +1,15 @@
+const express = require('express');
 const exphbs = require('express-handlebars');
+const path = require('path');
 
 module.exports = (app) => {
+    // directories
+    const static = path.join(__dirname, '../../static');
+
     // settings
     app.set('view engine', '.hbs');
     app.engine('.hbs', exphbs({ extname: '.hbs' }));
+
+    // static dir
+    app.use('/', express.static(static));
 }
